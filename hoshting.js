37f70f5115d2d138c8e8b8247a4d9feda81beae2
MO-ARBAZ <105699855+MO-ARBAZ@users.noreply.github.com>
2022-05-17 18:38:34 +0530

    Add files via upload

---
+++ hoshting.js
@@ -0,0 +1,71 @@
+/*var d = 'arybaz';
+console.log(d);
+*/
+/*
+tenMultiplier();
+tenMultiplier(5);
+function tenMultiplier(num){
+    console.log(num*10);
+}
+tenMultiplier();
+*/
+
+/*
+console.log(eightMultiplaer);//nan
+function eightMultiplaer(num){
+    console.log(num*18);
+}
+eightMultiplaer(10);
+*/
+
+
+//console.log(a);
+// console.log(tenMultiplier);
+// tenMultiplier(50);
+// var a =10;
+// var tenMultiplier = funtion(num){
+//     console.log(num*30);
+// }
+/**
+ * can be called before decleration becouze in memory creted;
+ * a function in memory  in memory creation phase;
+ * 
+ */
+// function tenMultiplier(num){
+//  console.log(num*20);
+// }
+
+// memory creation phase GES/Gs
+/*
+a=10;
+tenMultiplier= function(num){
+    console.log(num*20);
+}
+*/
+/**
+ * code excution phase
+ * EC for twentyMultiplier
+ * num =10;
+ * 
+ * 
+ * will be remove from call stack
+ * 
+ * printFullName()
+ */
+var firstName = 'name Arbaz'
+var lastName = 'lastname Shaikh';
+
+printFull()
+var printFull= function(){
+    console.log(firstName, lastName);
+}
+// printFull()
+
+function printFullName(){
+    console.log(firstName, lastName);
+}
+printFull();
+/**need to funtion 
+ * like a machine
+ * typing machine is like a funcation;
+ */
